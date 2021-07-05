58826eb14b173bf2c27ed0115b908ad6cd0e8416
Carlos Eduardo Caballero Burgoa <ccaballero@ehcgroup.io>
2021-07-05 03:47:46 -0400

    completed audios for expo 5 and 6

--- FIS102/Exposición 05/animaciones/src/index.js
+++ FIS102/Exposición 05/animaciones/src/index.js
@@ -1,5 +1,32 @@
-import {render} from 'react-dom';
-import App from './components/App';
+import './styles/index.css';
 
-render(<App />,document.getElementById('root'));
+const wave_f=(A,k,x,w,t)=>{
+    return A*Math.cos((k*x)-(w*t));
+}
+
+let svg=document.getElementById('wave'),
+    origin={
+        x:20,
+        y:128
+    },
+    path=document.createElementNS('http://www.w3.org/2000/svg','path');
+
+svg.appendChild(path);
+
+let k=0.04,
+    x=0,
+    w=2300,
+    t=0;
+
+setInterval(()=>{
+    let d=['M'];
+
+    for(let i=x;i<1880;i++){
+        d.push(origin.x+(i*1));
+        d.push(origin.y+wave_f(120,k,i,w,t));
+    }
+
+    path.setAttribute('d',d.join(' '));
+    t++;
+},(1000/12));
 
