81f1cf5d3d57618659cdf8813bbf5ff625237eb4
Carlos Eduardo Caballero Burgoa <ccaballero@ehcgroup.io>
2021-07-06 22:41:17 -0400

    added report 05

--- FIS102/Exposición 05/animaciones/src/index.js
+++ FIS102/Exposición 05/animaciones/src/index.js
@@ -1,32 +1,41 @@
-import './styles/index.css';
+import SVG from './img/v.svg';
+import './index.css';
 
 const wave_f=(A,k,x,w,t)=>{
     return A*Math.cos((k*x)-(w*t));
 }
 
-let svg=document.getElementById('wave'),
-    origin={
+let origin={
         x:20,
         y:128
     },
-    path=document.createElementNS('http://www.w3.org/2000/svg','path');
+    path=[
+        document.getElementById('wave1'),
+        document.getElementById('wave2'),
+        document.getElementById('wave3'),
+        document.getElementById('wave4'),
+        document.getElementById('wave5'),
+        document.getElementById('wave6')
+    ];
 
-svg.appendChild(path);
-
-let k=0.04,
+let A=64,
     x=0,
-    w=2300,
-    t=0;
+    t=0,
+    k=[6,5,4,3,2,1],
+    w=[1,2,3,4,5,6];
 
 setInterval(()=>{
-    let d=['M'];
+    for(let i=0;i<path.length;i++){
+        let d=['M'];
 
-    for(let i=x;i<1880;i++){
-        d.push(origin.x+(i*1));
-        d.push(origin.y+wave_f(120,k,i,w,t));
+        for(let j=x;j<1880;j++){
+            d.push(origin.x+(j*1));
+            d.push(-32+origin.y+(i*174)-wave_f(A,k[i]/100,j,w[i],t));
+        }
+
+        path[i].setAttribute('d',d.join(' '));
     }
 
-    path.setAttribute('d',d.join(' '));
     t++;
 },(1000/12));
 
