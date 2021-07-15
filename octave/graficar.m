05b8584d088dbe945e0903f9bd4c32d2c0755e69
Carlos Eduardo Caballero Burgoa <ccaballero@ehcgroup.io>
2021-07-15 03:32:36 -0400

    corrections on octave

--- octave/graficar.m
+++ octave/graficar.m
@@ -1,12 +1,15 @@
-function graficar(t,_x,_y,x,y)
-    % personalizar grafica
+function graficar(t,_x,_y,eps,x,y)
+    f = figure()
+
     title(t)
     xlabel(_x)
     ylabel(_y)
 
-    % graficar puntos y lineas
     hold on
+    grid on
     plot(x, y, 'o')
+
+    print(f,eps,'-color')
     hold off
 end
 
